6fae074985cb0564a7b88077c841ebea18a4fb2b
lyomu <gmnyomu@gmail.com>
2022-12-07 15:03:30 -0800

    New changes

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
-from models.engine.file_storage import FileStorage
+""" Determines storage method upon first instantiation of any
+`BaseModel`-derived object.
+"""
+from os import getenv
 
 
-storage = FileStorage()
-storage.reload()
+if getenv('HBNB_TYPE_STORAGE') == 'db':
+    from.engine.db_storage import DBStorage
+    storage = DBStorage()
+    storage.reload()
+
+else:
+    from .engine.file_storage import FileStorage
+    storage = FileStorage()
+    storage.reload()
